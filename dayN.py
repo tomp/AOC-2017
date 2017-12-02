4aa386f54b1cfe475abfe4f05fb1a63f4419be43
Tom Pollard <pollard.tom@gmail.com>
2017-12-02 17:22:40 -0500

    boilerplate code

---
+++ dayN.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#
+#  Advent of Code 2017 - Day N
+#
+
+INPUTFILE = 'input.txt'
+
+def load_input(infile):
+    lines = []
+    with open(infile, 'r') as fp:
+        for line in fp:
+            line = line.strip()
+            if line:
+                lines.append(line)
+        return lines
+
+# Example
+
+def example():
+    print('= ' * 32)
+
+# PART 1
+
+def part1(lines):
+    print('= ' * 32)
+
+
+# PART 2
+
+def part2(lines):
+    print('= ' * 32)
+
+if __name__ == '__main__':
+    example()
+    # input = load_input(INPUTFILE)
+    # part1(input)
+    # part2(input)
